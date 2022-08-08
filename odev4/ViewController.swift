2acd93a9284512675da9269eaae48279b9a15e17
KeremCaan <keremcaan@hotmail.com>
2022-08-08 18:38:35 +0300

    Initial Commit

---
+++ odev4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  odev4
+//
+//  Created by MacbookPro on 8.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
